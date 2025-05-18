5f31313d1087570ce4fb57b0fe1d7cf80bb123dc
WebSiteMaker <50154980+WebSiteMaker24@users.noreply.github.com>
2025-05-18 19:08:13 +0200

    Windows_InstallPepinPHP.c

--- Windows_InstallPepinPHP.c
+++ Windows_InstallPepinPHP.c
@@ -4,39 +4,64 @@
 
 int main()
 {
-    char projectName[256];
-    printf("Entrez le nom du projet (ex: WebSiteMaker) : ");
-    fgets(projectName, sizeof(projectName), stdin);
+  char projectName[256];
+  printf("Entrez le nom du projet (ex: WebSiteMaker) : ");
+  fgets(projectName, sizeof(projectName), stdin);
 
-    // Enlever le retour chariot final
-    size_t len = strlen(projectName);
-    if (len > 0 && projectName[len - 1] == '\n')
-    {
-        projectName[len - 1] = '\0';
-    }
+  // Enlever le retour chariot final
+  size_t len = strlen(projectName);
+  if (len > 0 && projectName[len - 1] == '\n')
+  {
+    projectName[len - 1] = '\0';
+  }
 
-    if (strlen(projectName) == 0)
-    {
-        strcpy(projectName, "PepinPHP");
-    }
+  if (strlen(projectName) == 0)
+  {
+    strcpy(projectName, "PepinPHP");
+  }
 
-    system("cd %USERPROFILE%\\Desktop && rmdir /s /q PepinPHP");
+  // Supprimer un ancien dossier PepinPHP s'il existe
+  system("cd %USERPROFILE%\\Desktop && rmdir /s /q PepinPHP");
 
-    char gitCmd[512];
-    snprintf(gitCmd, sizeof(gitCmd), "cd %%USERPROFILE%%\\Desktop && git clone https://github.com/WebSiteMaker24/PepinPHP.git PepinPHPTemp");
-    system(gitCmd);
+  // Cloner le dépôt
+  char gitCmd[512];
+  snprintf(gitCmd, sizeof(gitCmd),
+           "cd %%USERPROFILE%%\\Desktop && git clone https://github.com/WebSiteMaker24/PepinPHP.git PepinPHPTemp");
+  system(gitCmd);
 
-    char renameCmd[512];
-    snprintf(renameCmd, sizeof(renameCmd), "cd %%USERPROFILE%%\\Desktop && ren PepinPHPTemp %s", projectName);
-    system(renameCmd);
+  // Renommer le dossier cloné avec le nom du projet
+  char renameCmd[512];
+  snprintf(renameCmd, sizeof(renameCmd),
+           "cd %%USERPROFILE%%\\Desktop && ren PepinPHPTemp %s", projectName);
+  system(renameCmd);
 
-    char openCodeCmd[512];
-    snprintf(openCodeCmd, sizeof(openCodeCmd), "cd %%USERPROFILE%%\\Desktop\\%s && code .", projectName);
-    system(openCodeCmd);
+  // Renommer le fichier env.txt en .env
+  char renameEnvCmd[512];
+  snprintf(renameEnvCmd, sizeof(renameEnvCmd),
+           "cd %%USERPROFILE%%\\Desktop\\%s && ren env.txt .env", projectName);
+  system(renameEnvCmd);
 
-    char phpCmd[512];
-    snprintf(phpCmd, sizeof(phpCmd), "cd %%USERPROFILE%%\\Desktop\\%s && php -S localhost:8000 -t public_html/public", projectName);
-    system(phpCmd);
+  // Ouvrir le projet dans VS Code
+  char openCodeCmd[512];
+  snprintf(openCodeCmd, sizeof(openCodeCmd),
+           "cd %%USERPROFILE%%\\Desktop\\%s && code .", projectName);
+  system(openCodeCmd);
 
-    return 0;
+  // Ouvrir le navigateur sur index.php et lancer le serveur PHP
+  char phpCmd[1024];
+  snprintf(phpCmd, sizeof(phpCmd),
+           "cd %%USERPROFILE%%\\Desktop\\%s && start http://localhost:8000/index.php && php -S localhost:8000 -t public_html", projectName);
+  system(phpCmd);
+
+  return 0;
 }
+
+/*
+Compilation (Windows) :
+  gcc installPepinPHP.c -o installPepinPHP.exe
+
+Prérequis :
+  - MinGW installé (ou TDM-GCC)
+  - Le dossier "bin" de GCC dans le PATH
+  - Le fichier .exe pourra ensuite être lancé par double-clic
+*/
